11e1b3387f5dea7f0e126755c961433c0e9f984e
Jordi Kloosterboer <vetaz@outlook.com>
2026-04-16 20:11:35 -0600

    Update to latest

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,21 +1,4 @@
-// global.d.ts (or src/types/temporal.d.ts)
-
-import '@js-temporal/polyfill'
-import type { Temporal as TemporalPolyfill } from '@js-temporal/polyfill'
-
-declare global {
-  const Temporal: typeof TemporalPolyfill
-  namespace Temporal {
-    export type PlainDate = TemporalPolyfill.PlainDate
-    export type PlainMonthDay = TemporalPolyfill.PlainMonthDay
-    export type PlainTime = TemporalPolyfill.PlainTime
-    export type PlainDateTime = TemporalPolyfill.PlainDateTime
-    export type ZonedDateTime = TemporalPolyfill.ZonedDateTime
-    export type Duration = TemporalPolyfill.Duration
-    export type Instant = TemporalPolyfill.Instant
-  }
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- declaration merging
-  interface Window {
-    DO_NOT_AUTO_RUN_SCRAPERS?: boolean
-  }
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- declaration merging
+interface Window {
+  DO_NOT_AUTO_RUN_SCRAPERS?: boolean
 }
